1d9364e6ea1f6dbba48db8e9c66d74a44d44bd4f
Mike <mike.d.mcdougal@gmail.com>
2017-09-15 13:53:20 -0500

    final upload

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,68 +1,77 @@
 $(document).ready(function() {
-  // Create Initial Arrary Called Dogs
-  var dogs = ["Dalmatian", "Border Collie", "German Shorthaired Pointer", "Labrador Retriever", "Shiba Inu"];
+    // Make Dog Array -- Make newDogClick variable -- Make startGif variable
+    var dogs = ["Dalmatian", "Border Collie", "Alaskan Malamute", "Labrador Retriever", "Shiba Inu"];
+    var newDogClick;
+    var startGif = 0;
 
-  // Create Starting Buttons Function
-  function renderButtonsZero () {
-    $("#dogs-view").empty();
-    for (var i = 0; i < dogs.length; i++) {
-      var a = $("<button>");
-      // Adding a class
-      a.addClass("dog");
-      // Adding a data-attribute with a value of the movie at index i
-      a.attr("data-name", dogs[i]);
-      // Providing the button's text with a value of the movie at index i
-      a.text(dogs[i]);
-      // Adding the button to the HTML
-      $("#dogs-view").append(a);
+    // Create Start Button Function
+    function renderButtonsZero() {
+        $("#dog-buttons").empty();
+        for (var i = 0; i < dogs.length; i++) {
+            var a = $("<button>");
+            a.addClass("createDogGifs");
+            a.data("dogs", dogs[i]);
+            a.text(dogs[i]);
+            $("#dog-buttons").append(a);
+        }
     }
-  }
 
-  // Add New Buttons to Starting Buttons
-  $("#add-dog").on("click", function(event) {
-    // event.preventDefault() prevents the form from trying to submit itself.
-    // We're using a form so that the user can hit enter instead of clicking the button if they want
-    event.preventDefault();
-    // Take Text from Text Box
-    var dog = $("#dogs-input").val().trim();
-    // Add dog breed input into text box to the "dogs" array
-    dogs.push(dog);
-    // Call RenderButtonsZero Function to Display Newly Added Button
+    // Add New Button to Start Buttons
+    $("#add-dog").on("click", function(event) {
+        // event.preventDefault() prevents the form from trying to submit itself.
+        // We're using a form so that the user can hit enter instead of clicking the button if they want
+        event.preventDefault();
+        // Take Text from Text Box
+        var newDog = $("#dogs-input").val().trim();
+        if (newDog != "") {
+            // Add dog breed input into text box to the "dogs" array and prevent blank buttons
+            dogs.push(newDog);
+            // Call RenderButtonsZero Function to Display Newly Added Button
+            renderButtonsZero();
+        }
+
+    });
+
+    // Listen for Button Clicks and Download Gif
+    $(document).on("click", ".createDogGifs", function() {
+        newDogClick = $(this).data("dogs");
+        $("#dog-gallery").empty();
+        $("#dog-gallery").append("<h2>" + newDogClick +"<h2>");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            encodeURI(newDogClick) + "&api_key=dc6zaTOxFJmzC&limit=10&rating=g";
+        $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).done(function(response) {
+                for (var j = 0; j < response.data.length; j++) {
+                    var gifDiv = $("<h2>");
+                    var img = $("<img>");
+                    $(gifDiv).addClass("Canvas <h2>");
+                    $(img).attr("src", response.data[j].images.fixed_width_still.url);
+                    $(img).data("giphyData", response.data[j]);
+                    $(img).addClass("Start");
+                    $(gifDiv).append(img);
+                    $("#dog-gallery").append(gifDiv);
+                }
+            });
+        console.log(queryURL);
+    });
+
+    // Use startGif variable to track binary status of Gifs
+    $(document).on("click", ".Start", function() {
+        if (startGif === 0) {
+          startGif = 1;
+          $(this).attr("src", $(this).data("giphyData").images.fixed_width.url);
+          return;
+        }
+
+        if (startGif === 1) {
+            $(this).attr("src", $(this).data("giphyData").images.fixed_width_still.url);
+            startGif = 0;
+        }
+       
+    });
+
     renderButtonsZero();
-  });
 
-  // Listen for Button Clicks and Download Gifs
-  $(document).on("click", ".dog", function(event) {
-    var dog = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        dog + "&api_key=dc6zaTOxFJmzC&limit=10";
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-    .done(function(response) {
-          var results = response.data;
-          for (var j = 0; j < results.length; j++) {
-           // var gifDiv = $("<div class='item'>");
-
-           // var rating = results[j].rating;
-
-           // var p = $("<p>").text("Rating: " + rating);
-
-           // var personImage = $("<img>");
-           // personImage.attr("src", results[j].images.fixed_height.url);
-
-           // gifDiv.prepend(p);
-           // gifDiv.prepend(personImage);
-
-           // $("#gifs-appear-here").prepend(gifDiv);
-          }
-        });
-   	console.log(queryURL);
-  });
-
-  renderButtonsZero();  
- 
 });
-
-  
